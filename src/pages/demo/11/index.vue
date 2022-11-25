1a93fd3dada2b6da9070f49d2adaf5f59d5828b2
unknown <alexzarkk@hotmail.com>
2022-11-25 11:17:09 +0800

    a

--- src/pages/demo/11/index.vue
+++ src/pages/demo/11/index.vue
@@ -1,21 +1,19 @@
 <template>
 	<div>
-		<view class="flex justify-center margin">
-			<el-button type="primary" @click="detail('north')">北线</el-button>
-			<el-button type="primary" @click="detail('east')">东线</el-button>
-			<el-button type="primary" @click="detail('south')">南线</el-button>
-			<el-button type="primary" @click="detail('west')">西线</el-button>
-			<el-button type="primary" @click="detail('zs')">舟山</el-button>
-		</view>
+		<view class="flex justify-center">
+			<el-button type="primary" size="mini" @click="detail('north')">北线</el-button>
+			<el-button type="primary" size="mini" @click="detail('east')">东线</el-button>
+			<el-button type="primary" size="mini" @click="detail('south')">南线</el-button>
+			<el-button type="primary" size="mini" @click="detail('west')">西线</el-button>
+			<el-button type="primary" size="mini" @click="detail('zs')">舟山</el-button>
 		
-		<view class="flex justify-center margin">
-			<el-button type="success" @click="show(['north'])">北线</el-button>
-			<el-button type="success" @click="show(['east'])">东线</el-button>
-			<el-button type="success" @click="show(['south'])">南线</el-button>
-			<el-button type="success" @click="show(['west'])">西线</el-button>
-			<el-button type="success" @click="show(['zs'])">舟山</el-button>
-			<el-button type="success" @click="show(['dt'])">洞头</el-button>
-			<el-button type="success" @click="show(['north','east','south','west','zs','dt'])">全省</el-button>
+			<el-button type="success" size="mini" @click="show(['north'])">北线</el-button>
+			<el-button type="success" size="mini" @click="show(['east'])">东线</el-button>
+			<el-button type="success" size="mini" @click="show(['south'])">南线</el-button>
+			<el-button type="success" size="mini" @click="show(['west'])">西线</el-button>
+			<el-button type="success" size="mini" @click="show(['zs'])">舟山</el-button>
+			<el-button type="success" size="mini" @click="show(['dt'])">洞头</el-button>
+			<el-button type="success" size="mini" @click="show(['north','east','south','west','zs'])">全省</el-button>
 		</view>
 			
 		<map11 v-if="loaded" :pms="kml" :winH="800"></map11>
