268210c129dccb4f5c650f54e291d6c0be4ee162
InsHomePgup <7328234@gmail.com>
2022-12-13 14:12:03 +0800

    首页轮播图管理

---
+++ src/pages/template/table-curd.vue
@@ -0,0 +1,64 @@
+<template>
+    <cl-layout>
+        <div class="system-user">
+            <div class="pane">
+                <div class="user">
+                    <cl-crud ref="crud" @load="onCrudLoad" boder>
+                        <el-row type="flex">
+                            <cl-table :columns="columns">
+                                <!-- 时间 -->
+                                <template #column-createTime="{ scope }">
+                                    {{ scope.row.updateTime }}
+                                </template>
+                            </cl-table>
+                        </el-row>
+
+                        <el-row type="flex">
+                            <cl-flex1 />
+                            <cl-pagination />
+                        </el-row>
+                    </cl-crud>
+
+                </div>
+            </div>
+        </div>
+    </cl-layout>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            columns: [
+                {
+                    type: 'selection',
+                    align: 'center',
+                    width: '60'
+                },
+                {
+                    prop: 'deptId',
+                    label: '部门',
+                    align: 'center',
+                    dict: depts
+                },
+                {
+                    label: '操作',
+                    align: 'center',
+                    type: 'op',
+                    buttons: ['slot-edit', 'slot-detail', 'slot-veri']
+                }
+            ]
+        }
+    },
+    watch: {},
+    mounted() {
+    },
+    methods: {
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
+
--- src/pages/zts/home/banner.vue
+++ src/pages/zts/home/banner.vue
@@ -1,7 +1,35 @@
 <template>
     <!-- 首页 - 轮播图管理 -->
     <cl-layout>
-        <div>123</div>
+        <div class="system-user">
+            <div class="pane">
+                <div class="user">
+                    <cl-crud ref="crud" @load="onCrudLoad" boder>
+                        <el-row type="flex">
+                            <cl-refresh-btn />
+                            <cl-add-btn></cl-add-btn>
+                            <!-- <el-button size="mini" type="primary">新增</el-button> -->
+                            <cl-multi-delete-btn />
+                        </el-row>
+                        <el-row type="flex">
+                            <cl-table :columns="columns">
+                                <!-- 时间 -->
+                                <!-- <template #column-createTime="{ scope }">
+                                    {{ scope.row.updateTime }}
+                                </template> -->
+                            </cl-table>
+                        </el-row>
+
+                        <el-row type="flex">
+                            <cl-flex1 />
+                            <cl-pagination />
+                        </el-row>
+                        <!-- 轮播图编辑表单 -->
+                        <cl-upsert ref="upsert" v-bind="upsert.props" :items="upsert.items" :on-submit="onUpsertSubmit" @open="onUpsertOpen"></cl-upsert>
+                    </cl-crud>
+                </div>
+            </div>
+        </div>
     </cl-layout>
 </template>
 
@@ -10,12 +38,138 @@
 export default {
     data() {
         return {
+            columns: [
+                {
+                    type: 'selection',
+                    align: 'center',
+                    width: '60'
+                },
+                {
+                    prop: 'title',
+                    label: '标题',
+                    align: 'center'
+                },
+                {
+                    prop: 'remark',
+                    label: '备注',
+                    align: 'center'
+                },
+                {
+                    prop: 'remark',
+                    label: '是否加入轮播图展示',
+                    align: 'center'
+                },
+                {
+                    prop: 'cover',
+                    label: '封面图/展示的图片',
+                    align: 'center'
+                },
+                {
+                    prop: 'cover',
+                    label: '目标类型',  // 文章  /  专题 / 赛事  / 活动
+                    align: 'center'
+                },
+                {
+                    prop: 'cover',
+                    label: '指定编号',
+                    align: 'center'
+                },
+                {
+                    label: '操作',
+                    align: 'center',
+                    type: 'op',
+                    buttons: ['']
+                }
+            ],
+            upsert: {
+                props: {
+                    props: {
+                        width: "1000px",
+                        // "label-position": "top",
+                        "label-width": "15em"
+                    }
+                },
+                items: [
+                    {
+                        prop: "title",
+                        label: "标题",
+                        span: 24,
+                        component: {
+                            name: "el-input",
+                            attrs: {
+                                placeholder: "请输入轮播图标题"
+                            }
+                        }
+                    },
+                    {
+                        prop: "remark",
+                        label: "备注",
+                        span: 24,
+                        component: {
+                            name: "el-input"
+                        }
+                    },
+                    {
+                        prop: "isShow",
+                        label: "是否加入轮播图展示",
+                        span: 24,
+                        component: {
+                            name: "el-switch"
+                        }
+                    },
+                    {
+                        prop: "cover",
+                        label: "封面图",
+                        span: 24,
+                        component: {
+                            name: "cl-upload"
+                        },
+                        rules: {
+                            required: true
+                        }
+                    },
+                    {
+                        label: "轮播图类型",
+                        prop: "type",
+                        component: {
+                            name: "el-select",
+                            options: [
+                                {
+                                    label: "专题",
+                                    value: 1
+                                },
+                                {
+                                    label: "文章",
+                                    value: 2
+                                },
+                                {
+                                    label: "活动",
+                                    value: 3
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        label: "传递编号",
+                        prop: "contentId",
+                        component: {
+                            name: "el-input"
+                        }
+                    }
+                ]
+            }
         }
     },
     watch: {},
     mounted() {
     },
     methods: {
+        onUpsertOpen() {
+
+        },
+        onUpsertSubmit() {
+
+        }
     }
 };
 </script>
